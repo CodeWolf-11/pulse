100df8604a88b14e24bfe8a0b3f2cba90e1d5b57
Nishant Rai <n.rai.code@outlook.com>
2024-09-26 20:49:22 +0530

    dark mode added for footer

--- client/src/components/base/Footer.tsx
+++ client/src/components/base/Footer.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Footer() {
     return (
-        <footer className="p-6 bg-gray-900 text-white">
+        <footer className="p-6 bg-gray-900 text-white dark:bg-black">
             <div className="flex justify-between">
                 <div>
                     <div>© 2024 Pulse. All rights reserved.</div>
