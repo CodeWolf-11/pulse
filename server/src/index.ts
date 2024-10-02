2aed480406468f82169f5281b255ddb3d7e8b63d
Nishant Rai <n.rai.code@outlook.com>
2024-10-02 15:34:42 +0530

    added redis streams adapter to socket.io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,14 @@ import "dotenv/config";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import { socketConfig } from "./socket";
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import redis from "./config/redis.config";
 
 const app = Express();
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: false }));
+app.use("/api", routes);
 const PORT = Number(process.env.PORT) || 5000;
 
 
@@ -14,17 +20,15 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*"
-    }
+    },
+    adapter: createAdapter(redis)
 });
 
 socketConfig(io);
 
 export { io };
 
-app.use(cors());
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: false }));
-app.use("/api", routes);
+
 
 server.listen(PORT, () => {
     console.log("Server is running on port ", PORT)
